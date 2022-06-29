67b5a73c494bbcb8551d46f8928d04525c05de75
fabien <fabien.j.vincent@orange.fr>
2022-06-29 15:03:51 +0200

    ajout TU de test pour eviter regression appli existante

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -16,4 +16,31 @@ class ShoppingControllerTests extends UnitTest {
             () -> controller.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
         );
     }
+    
+    @Test
+    void should_equal_mock_standard_customer() {
+    	Item[] items = new Item[3];
+    	items[0] = new Item("TSHIRT", 1);
+    	items[1] = new Item("DRESS", 1);
+    	items[2] = new Item("JACKET", 1);
+        Assertions.assertEquals(controller.getPrice(new Body(items, "STANDARD_CUSTOMER")), "180.0");
+    }
+    
+    @Test
+    void should_equal_mock_premium_customer() {
+    	Item[] items = new Item[3];
+    	items[0] = new Item("TSHIRT", 2);
+    	items[1] = new Item("DRESS", 2);
+    	items[2] = new Item("JACKET", 2);
+        Assertions.assertEquals(controller.getPrice(new Body(items, "PREMIUM_CUSTOMER")), "324.0");
+    }
+    
+    @Test
+    void should_equal_mock_platinum_customer() {
+    	Item[] items = new Item[3];
+    	items[0] = new Item("TSHIRT", 2);
+    	items[1] = new Item("DRESS", 3);
+    	items[2] = new Item("JACKET", 2);
+        Assertions.assertEquals(controller.getPrice(new Body(items, "PLATINUM_CUSTOMER")), "205.0");
+    }
 }
